14129fe2677d0604fe5e10fa2b4ad1e0dc43d99a
Shaun Mulligan <switchflip.shaun@gmail.com>
2017-08-28 13:18:15 +0100

    add network checking

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-
-const SerialPort = require('serialport');
-
-
-SerialPort.list(function (err, ports) {
-  ports.forEach(function(port) {
-    console.log(port.comName);
-  });
-});
-
+const isOnline = require('is-online');
+const blinkt = require('blinkt');
+ 
 console.log('Hello, world!');
-
+blinkt.clear();
 function intervalFunc () {
   console.log('Cant stop me now!');
+  isOnline().then(online => {
+    console.log("device connected to the internet")
+    blinkt.setPixels(0, 255, 0, 1);
+    blinkt.show();
+  })
+  .catch(err => {
+    console.log("ERROR: no internet")
+    blinkt.setPixels(255, 0, 0, 1);
+    blinkt.show();
+  });
 }
 
 setInterval(intervalFunc, 1500);
