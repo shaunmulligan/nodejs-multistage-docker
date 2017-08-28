bbcb7fbce6eee3333d810baba72e5bce92daf50b
Shaun Mulligan <switchflip.shaun@gmail.com>
2017-08-28 13:47:33 +0100

    add if(online) test

--- main.js
+++ main.js
@@ -5,16 +5,19 @@ const blinkt = new Blinkt({defaultBrightness: 0.5});
 console.log('Hello, world!');
 blinkt.off();
 function intervalFunc () {
-  console.log('Cant stop me now!');
+  console.log('Testing connection...!');
   isOnline().then(online => {
-    console.log("device connected to the internet")
-    blinkt.setAll(0, 255, 0);
+    if(online){
+      console.log("device connected to the internet")
+      blinkt.setAll(0, 255, 0);
+    }else {
+      console.log("ERROR: no internet")
+      blinkt.setAll(255, 0, 0);
+    }
     blinkt.draw();
   })
   .catch(err => {
-    console.log("ERROR: no internet")
-    blinkt.setAll(255, 0, 0);
-    blinkt.draw();
+    console.log("ERROR: something weird happened")
   });
 }
 
